949fca38cc98ff58263115eb3ee86c098ceed5d7
felocSsS <vlad.shipilov.felocs@gmail.com>
2020-09-27 22:55:18 +0300

    first commit

---
+++ ВзломПентагона/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ВзломПентагона
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Playbill", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(591, 62);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Взлом Пентагона";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(591, 44);
+            this.label2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(173, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 116);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Взломать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(0, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(591, 67);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(0, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(591, 74);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(0, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(591, 74);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(0, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(591, 149);
+            this.label6.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 295);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(211, 23);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 221);
+            this.progressBar2.MarqueeAnimationSpeed = 0;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(211, 23);
+            this.progressBar2.TabIndex = 8;
+            this.progressBar2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(591, 645);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hack of the pentagon";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
+
---
+++ ВзломПентагона/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ВзломПентагона
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        int i = 0;
+        int k = 0;
+        int j = 0;
+        int y = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+            switch (k)
+            {
+                case 0:
+                    if (j == 0)
+                    {
+                        label2.Text = ("Подбор ip адреса: " + i).ToString();
+                        if (i == 195)
+                        {
+                            j = 1;
+                            i = 0;
+                        }
+                    }
+                    if (j == 1)
+                    {
+                        label2.Text = ("Подбор ip адреса: 195." + i).ToString();
+                            if (i == 243)
+                            {
+                                j = 2;
+                                i = 0;
+                            }
+                    }
+                    if (j == 2)
+                    {
+                        label2.Text = ("Подбор ip адреса: 195.243." + i).ToString();
+                        if (i == 248)
+                        {
+                            j = 3;
+                            i = 0;
+                        }
+                    }
+                    if (j == 3)
+                    {
+                        label2.Text = ("Подбор ip адреса: 195.243.248." + i).ToString();
+                        if (i == 192)
+                        {
+                            k = 1;
+                            i = 0;
+                        }
+                    }
+
+                    break;
+                case 1:
+                    timer1.Interval = 150;
+                    if (y == 0) 
+                    {
+                        label3.Text = ("Подбор пороля:\n") + i + "*******";
+                        if (i == 8)
+                        {
+                            i = 0;
+                            y = 1;
+                        }    
+                    }
+                    if (y == 1)
+                    {
+                        label3.Text = ("Подбор пороля:\n8") + i + "******";
+                        if (i == 9)
+                        {
+                            i = 0;
+                            y = 2;
+                        }
+                    }
+                    if (y == 2)
+                    {
+                        label3.Text = ("Подбор пороля:\n89") + i + "*****";
+                        if (i == 4)
+                        {
+                            i = 0;
+                            y = 3;
+                        }
+                    }
+                    if (y == 3)
+                    {
+                        label3.Text = ("Подбор пороля:\n894") + i + "****";
+                        if (i == 0)
+                        {
+                            i = 0;
+                            y = 4;
+                        }
+                    }
+                    if (y == 4)
+                    {
+                        label3.Text = ("Подбор пороля:\n8940") + i + "***";
+                        if (i == 7)
+                        {
+                            i = 0;
+                            y = 5;
+                        }
+                    }
+                    if (y == 5)
+                    {
+                        label3.Text = ("Подбор пороля:\n89407") + i + "**";
+                        if (i == 3)
+                        {
+                            i = 0;
+                            y = 6;
+                        }
+                    }
+                    if (y == 6)
+                    {
+                        label3.Text = ("Подбор пороля:\n894073") + i + "*";
+                        if (i == 5)
+                        {
+                            i = 0;
+                            label3.Text = "Подбор пороля:\n8940735";
+                            k = 2;
+                        }
+                    }
+                    break;
+                case 2:
+                    progressBar2.Visible = true;
+                    progressBar2.Value += 1;
+                    label4.Text = "Процесс взлома: " + i + "%";
+                    if (i == 100)
+                    {
+                        i = 0;
+                        k = 3;
+                    }
+                    break;
+                case 3:
+                    timer1.Interval = 50;
+                    progressBar1.Visible = true;
+                    progressBar1.Value += 1;
+                    label5.Text = "Скачивание данных: " + i + "%";
+                    if (i == 100)
+                    {
+                        i = 0;
+                        k = 4;
+                    }
+                    break;
+                case 4:
+                    timer1.Interval = 1000;
+                    label6.Text = "Полученные данные:\nИСИП - 15 тупа лучшие хацкеры";
+                    break;
+        }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
